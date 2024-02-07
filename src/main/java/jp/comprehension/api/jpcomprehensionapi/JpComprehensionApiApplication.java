b84459d0746e25d39ae29d6548a67a11e407bba8
mrhul <mr.huliganer@gmail.com>
2024-02-07 13:30:48 +0100

    Project setup + architecture diagram

---
+++ src/main/java/jp/comprehension/api/jpcomprehensionapi/JpComprehensionApiApplication.java
@@ -0,0 +1,13 @@
+package jp.comprehension.api.jpcomprehensionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpComprehensionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpComprehensionApiApplication.class, args);
+    }
+
+}
