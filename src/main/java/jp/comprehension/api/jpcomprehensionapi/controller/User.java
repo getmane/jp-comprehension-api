42ce3e3640d97c5e0dc204a5cccc06fc2f6e0a3f
mrhul <mr.huliganer@gmail.com>
2024-02-09 23:56:13 +0100

    Added entities + app configs

---
+++ src/main/java/jp/comprehension/api/jpcomprehensionapi/controller/User.java
@@ -0,0 +1,26 @@
+package jp.comprehension.api.jpcomprehensionapi.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@Document("users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+    private String email;
+
+    @DBRef(lazy = true)
+    private List<Vocab> vocabs;
+}
---
+++ src/main/java/jp/comprehension/api/jpcomprehensionapi/controller/Vocab.java
@@ -0,0 +1,26 @@
+package jp.comprehension.api.jpcomprehensionapi.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Document("vocabs")
+public class Vocab {
+
+    @Id
+    private String id;
+
+    private String spelling;
+    private String reading;
+
+    @DBRef(lazy = true)
+    private List<User> users;
+}
