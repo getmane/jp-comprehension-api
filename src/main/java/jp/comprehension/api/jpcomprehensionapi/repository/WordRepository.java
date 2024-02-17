6f86c09e63570dbfe75def350d5b68d6c9348e99
mrhul <mr.huliganer@gmail.com>
2024-02-17 23:44:20 +0100

    Added find word query

--- src/main/java/jp/comprehension/api/jpcomprehensionapi/repository/WordRepository.java
+++ src/main/java/jp/comprehension/api/jpcomprehensionapi/repository/WordRepository.java
@@ -4,6 +4,9 @@ import jp.comprehension.api.jpcomprehensionapi.domain.Word;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WordRepository extends MongoRepository<Word, String> {
+    Optional<Word> findFirstBySpellingAndReadingAndMeaning(String spelling, String reading, String meaning);
 }
