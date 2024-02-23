b1b9165ea132c5650bfc1e63d94fba1654f63085
mrhul <mr.huliganer@gmail.com>
2024-02-24 00:00:47 +0100

    added dto for jpdb import results

---
+++ src/main/java/jp/comprehension/api/jpcomprehensionapi/dto/user/jpdb/JpdbImportStat.java
@@ -0,0 +1,15 @@
+package jp.comprehension.api.jpcomprehensionapi.dto.user.jpdb;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JpdbImportStat {
+
+    private Integer wordsAdded;
+}
