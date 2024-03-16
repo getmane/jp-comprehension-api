237c25f7c1c3f845275ae0397d723a82ba322656
mrhul <mr.huliganer@gmail.com>
2024-03-16 23:51:22 +0100

    Extracted message for user creation exc

--- src/main/java/jp/comprehension/api/jpcomprehensionapi/exception/UserCreateException.java
+++ src/main/java/jp/comprehension/api/jpcomprehensionapi/exception/UserCreateException.java
@@ -5,7 +5,9 @@ import org.springframework.dao.DuplicateKeyException;
 
 public class UserCreateException extends DuplicateKeyException {
 
+    private static final String USER_NOT_CREATED_MESSAGE = "Could not create user. ";
+
     public UserCreateException(String msg) {
-        super(msg);
+        super(USER_NOT_CREATED_MESSAGE + msg);
     }
 }
